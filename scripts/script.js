5e6de4e6c58ccd3417537e19c71d649c4bd13c99
Pan Artur <artur.pankov.16@list.ru>
2020-03-12 22:23:44 +0300

    feat: add tab-sort

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,9 @@
 (() => {
     let SLIDER_COLORS = {
-        blue: '#648BF0',
-        red: '#f06c64'
+        BLUE: '#648BF0',
+        RED: '#f06c64'
     }
-
-
+    
     const sliderBackground = document.querySelector('.slider-section__background');
     const slideOne = document.getElementById('slide-one');
     const slideTwo = document.getElementById('slide-two');
@@ -12,21 +11,72 @@
     const rowRight = document.getElementById('row-right');
     let flag = true;
 
+    //slider
     let toSlide = () => {
         if (flag) {
-            sliderBackground.style.backgroundColor = SLIDER_COLORS.blue;
-            slideOne.style.opacity = '0';
-            slideTwo.style.opacity = '1';
-            console.log(slideTwo.style);
+            sliderBackground.style.backgroundColor = SLIDER_COLORS.BLUE;
+            slideOne.style.display = 'none';
+            slideTwo.style.display = 'block';
+            //slideOne.style.opacity = '0';
+            //slideTwo.style.opacity = '1';
             flag = false;
         } else {
-            sliderBackground.style.backgroundColor = SLIDER_COLORS.red;
-            slideOne.style.opacity = '1';
-            slideTwo.style.opacity = '0';
+            sliderBackground.style.backgroundColor = SLIDER_COLORS.RED;
+            //slideOne.style.opacity = '1';
+            //slideTwo.style.opacity = '0';
+            slideOne.style.display = 'block';
+            slideTwo.style.display = 'none';
             flag = true;
         }
     }
 
     rowLeft.addEventListener('click', toSlide);
     rowRight.addEventListener('click', toSlide);
+
+    //phone-activate
+
+    const sliderImageOne = document.querySelector('.slider-section__image-one');
+    const sliderImageTwo = document.querySelector('.slider-section__image-two');
+
+    let toColorVertical = (evt) => {
+        if (evt.offsetX <= 215 && evt.offsetY <= 458) {
+            sliderImageOne.firstChild.classList.add('black-vertical');
+        }
+    }
+
+    let toColorHorizontal = (evt) => {
+        if (evt.offsetX <= 458 && evt.offsetY <= 215) {
+            sliderImageTwo.firstChild.classList.add('black-horizontal');
+        }
+    }
+
+    sliderImageOne.addEventListener('click', toColorVertical);
+    sliderImageTwo.addEventListener('click', toColorHorizontal);
+
+    //portfolio start
+    const testArr = document.querySelectorAll('.image-list__list-item');
+    const portfolioNav = document.querySelectorAll('.portoflio-section__list-item');
+
+    let toReversImages = (evt) => {
+        evt.preventDefault();
+        let tess = '';
+        testArr.forEach((it, i, arr) => {
+            if (i === 0) {
+                tess = it.querySelector('img').src;
+            } 
+            if (i === arr.length - 1) {
+                it.querySelector('img').src = tess;
+            } else {
+                it.querySelector('img').src = arr[i + 1].querySelector('img').src;
+            }      
+            
+        });
+    }
+
+
+
+    portfolioNav.forEach((it) => {
+        it.addEventListener('click', toReversImages);
+    });    
 }) ()
+
